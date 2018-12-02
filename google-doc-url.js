76c03d0d76d35e5e28ca7355a932e7085483f5cd
shauxiao <44531645+shauxiao@users.noreply.github.com>
2018-12-01 21:58:07 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZnuELN4O6xAtto0x0wQKNAHU9tGbPdxsFKGQIuRSMOw/edit#gid=0';
